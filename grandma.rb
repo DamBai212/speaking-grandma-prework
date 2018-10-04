0195a8e953ef3d30b055e3ab3c22c34da1579998
DamBai212 <damanybailey@icloud.com>
2018-10-04 19:51:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.downcase
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == phrase.upcase
+  elsif phrase.upcase
     return 'NO, NOT SINCE 1938!'
   end
 end

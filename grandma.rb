e5b719405f19cd3b8d6b9ae720c4ae6ae152389a
DamBai212 <damanybailey@icloud.com>
2018-10-04 19:48:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase.lowercase
+  if phrase == phrase.downcase
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     return 'NO, NOT SINCE 1938!'
+  end
 end
 
 # Whatever you say to grandma, she should respond with
